2d3c50c9edfd455c52638e6f4a7ec3316d712a3b
Ehimare Okosun <ehimare369@gmail.com>
2020-10-23 18:53:40 +0100

    Edited app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+
 const App = () => {
     return(
         <div>
-            <h1>Hello World!</h1>
+            <h1>Hello { name }!</h1>
             <h3>From inisde src/app.js, which is bundled to public/bundle.js</h3>
-            <h3>This is just another begining</h3>
+            <h3>{name} use this as your simple react project boilerplate. More like your own create-react-app</h3>
         </div>
     )
 }
+
+const name = "Ehimare";
+const age = 2020 - 1989;
 
 render(<App />, document.getElementById('root'));
